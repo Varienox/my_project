67ff5311ba3873105296f15e94d14dcca225ff11
Mateusz Kucharzyk <mateusz.kucharzyk006@gmail.com>
2024-11-29 12:29:05 +0000

    styling tailwind

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -19,7 +19,7 @@ getChat()
 </script>
 
 <template>
-  <main class="container nx-auto">
+  <main class="container mx-auto flex justify-center items-center flex-col p-6 border-2 border-cyan-700 my-6 w-fit rounded-xl">
     
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
@@ -30,7 +30,7 @@ getChat()
       <button type="submit">Click Me!</button>
     </form>
     <section id="displayChat">
-      <div>{{ displayChat }}</div>
+      <div v-for="msg in displayChat" class="flex justify-start">{{ msg }}</div>
     </section>
   </main>
 </template>
